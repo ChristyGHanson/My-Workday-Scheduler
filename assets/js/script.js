409bdfabfcd1dc0ccd81ccab5965bf97f8d077e4
Christy G. Hanson <34938344+ChristyGHanson@users.noreply.github.com>
2023-02-02 19:27:03 -0700

    Updated version.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+
+
+var dateTimeNow = dayjs();
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +24,22 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+// Pseudocode starts here:
+// Q: How do I get the current day to display?
+// A: I need to use the Day.js library. I plug in this Day.js library into my HTML file.
+
+//  Q: Style.css color code. lines 33-36. What did we learn in JS that would be a conditional statement that would change each of these things? 
+//  A: Use If-else statement to change the past, present and future colors. 
+// Comparisons in code. 9am compare it to 12pm. (==)
+// Day.js is always going to track it for me. I have to compare the time blocks.
+//  If true, it's past, then do something with that class of past to change colors. 
+
+// when i click the save button... --> 
+
+//refreshing and event persist is going to be the hard part.
+// I have to learn to store the data I entered into the form. 
+// I have to store it in local storage.
+// If I hit refresh in the browser, the data must reappear and stay put. It will not disappear. 
+// I could use a for-loop or just store it. 
+
